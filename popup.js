7153172ef9589662584ab9aed6cab33d4030ae7c
Pavel Kolesnikov <olpyhn@yandex-team.ru>
2019-11-16 20:03:42 +0300

    small redesign, save state

--- popup.js
+++ popup.js
@@ -1,20 +1,24 @@
-var buttonEnabled = false;
-
-function setButtonAttrs(button) {
-    if (buttonEnabled) {
-        button.innerText = "TURN ON";
-        button.setAttribute("class", "button_enabled");
-        buttonEnabled = false;
+function update(checkbox) {
+    console.log(checkbox.checked);
+    storage.setItem("turned_on", checkbox.checked.toString());
+    if (checkbox.checked) {
+        document.getElementById("img_face").setAttribute("src", "happy.png");
     } else {
-        button.innerText = "TURN OFF";
-        button.setAttribute("class", "button_disabled");
-        buttonEnabled = true;
+        document.getElementById("img_face").setAttribute("src", "unhappy.png");
     }
 }
 
+let storage = window.localStorage;
 document.addEventListener('DOMContentLoaded', function() {
-    var turnOnButton = document.getElementById('turnOnButton');
-    turnOnButton.addEventListener('click', function() {
-        setButtonAttrs(turnOnButton);
-    }, false);
+    let checkbox = document.getElementById('on_off');
+    if (storage.getItem("turned_on") !== "true" && storage.getItem("turned_on") !== "false") {
+        storage.setItem("turned_on", "true");
+    }
+    checkbox.checked = (storage.getItem("turned_on") === "true");
+    if (checkbox.checked) {
+        document.getElementById("img_face").setAttribute("src", "happy.png");
+    } else {
+        document.getElementById("img_face").setAttribute("src", "unhappy.png");
+    }
+    checkbox.addEventListener('click', () => update(checkbox), false);
 }, false);

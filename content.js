b1c4d9d84f91de3d9df68313aea3bdba922c9a6a
Roman Zhikharevich <rzhikharevich@gmail.com>
2019-11-16 20:35:33 +0200

    123

--- content.js
+++ content.js
@@ -1,132 +1,153 @@
-let data = JSON.parse("{}");
-
-let sendText = (text, onResult) => {
-    const url = 'http://127.0.0.1:8080/detoxify';
-    xhr = new XMLHttpRequest();
-    xhr.open("POST", url, true);
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.send(JSON.stringify({"text": text}));
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            console.log(xhr);
-            onResult(xhr.responseText);
-        }
-        if (xhr.readyState === 4 && xhr.status !== 200) {
-            console.log(xhr.status + ': ' + xhr.statusText + ' error');
-        }
-    };
-};
-
-function updateElement(id) {
-    let text = data[id].text;
-    let spans = document.getElementsByTagName("span");
-    for (let i = 0; i < spans.length; i++) {
-        let item = spans.item(i);
-        if (text.includes(item.innerText) && (item.innerText.length / text.length > 0.8)) {
-            if (data[id].has_result) {
-                let result = data[id].response;
-                if (Object.keys(result).indexOf("text") >= 0) {
-                    item.innerText = result.text;
-                }
-            } else {
-                sendText(item.innerText, result => {
-                    data[id].has_result = true;
-                    data[id].response = JSON.parse(result);
-                    if (Object.keys(data[id].response).indexOf("text") >= 0) {
-                        item.innerText = data[id].response.text;
-                    }
-                });
-            }
-        }
-    }
-}
-
-function interceptData() {
-    const xhrOverrideScript = document.createElement('script');
-    xhrOverrideScript.type = 'text/javascript';
-    xhrOverrideScript.innerHTML = `
-(function() {
-    var XHR = XMLHttpRequest.prototype;
-    var send = XHR.send;
-    var open = XHR.open;
-    XHR.open = function(method, url) {
-        this.url = url;
-        return open.apply(this, arguments);
-    }
-    XHR.send = function() {
-        this.addEventListener('load', function() {
-            if (this.url.includes('https://api.twitter.com/2/timeline')) {
-                var dataDOMElement = document.createElement('div');
-                dataDOMElement.id = '__interceptedData';
-                dataDOMElement.innerText = this.response;
-                dataDOMElement.style.height = 0;
-                dataDOMElement.style.overflow = 'hidden';
-                document.body.appendChild(dataDOMElement);
-            }               
-        });
-        let event = new Event("onResponseInterception", {});
-        document.dispatchEvent(event);
-        return send.apply(this, arguments);
-    };
-})();
-    `;
-    document.head.prepend(xhrOverrideScript);
-}
-
-function processText(text) {
-    let words = text.split(" ");
-    text = "";
-    let isStart = true;
-    for (let word of words) {
-        if (word.slice(0, 1) === "@" && isStart) {
-            isStart = true;
-        } else {
-            if (word.slice(0, 4) === "http") {
-                isStart = false;
-                continue;
-            }
-            if (isStart) {
-                text = word;
-            } else {
-                text += ' ' + word;
-            }
-            isStart = false;
-        }
-    }
-    return text;
-}
-
-function checkForDOM() {
+function init() {
     if (document.body && document.head) {
-        interceptData();
+        patchXhr();
     } else {
-        requestIdleCallback(checkForDOM);
+        requestIdleCallback(init);
     }
 }
 
-function scrapeData() {
-    let divs = document.getElementsByTagName("div");
-    for (let i = 0; i < divs.length; i++) {
-        if (divs.item(i).getAttribute("id") === "__interceptedData") {
-            const response = JSON.parse(divs.item(i).innerHTML);
-            let tweets = response.globalObjects.tweets;
-            for (let key in tweets) {
-                if (Object.keys(data).indexOf(key) >= 0) {
-                    continue;
+function patchXhr() {
+    let shellcode = document.createElement("script");
+    shellcode.type = "text/javascript";
+
+    shellcode.innerHTML = `
+        const TWITTER_HOOK_PREFIX = "https://api.twitter.com/2/timeline/profile/";
+
+        let nextTweetId = 0;
+        let idToText = {};
+        let idToElement = {};
+
+        function hookProperty(obj, key, get_fun, set_fun) {
+            let value;
+
+            Object.defineProperty(
+                obj, key,
+                {
+                    enumerable: true,
+                    configurable: true,
+
+                    get() {
+                        return get_fun.call(this, value);
+                    },
+
+                    set(newValue) {
+                        value = set_fun.call(this, newValue);
+                    }
                 }
-                data[key] = {text: processText(tweets[key].full_text), has_result: false, response: ""};
-            }
-            divs.item(i).remove();
+            );
         }
-    }
-    for (let key in data) {
-        updateElement(key);
-    }
+
+        function detoxifyText(text, ) {
+            let xhr = new XMLHttpRequest();
+
+            xhr.onreadystatechange = () => {
+                if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
+                    let response = JSON.parse(xhr.responseText);
+
+                }
+            };
+        }
+
+        function walkTweets(tree, ids) {
+            if (typeof tree != "object") {
+                return;
+            }
+
+            for (let key in tree) {
+                if (key == "full_text") {
+                    ids.push(nextTweetId);
+                    idToText[nextTweetId] = tree[key];
+                    tree[key] = "__detoxify__" + nextTweetId.toString();
+                    nextTweetId++;
+                } else {
+                    walkTweets(tree[key]);
+                }
+            }
+        }
+
+        (function() {
+            let xhr = XMLHttpRequest.prototype;
+
+            let xhrOpen = xhr.open;
+
+            xhr.open = function(method, url) {
+                this.__detoxify__url = url;
+                return xhrOpen.apply(this, arguments);
+            };
+
+            let xhrGetResponseText = xhr.__lookupGetter__("responseText");
+
+            // hookProperty(
+            //     xhr, "responseText",
+
+            //     function(value) {
+            //         if (value == undefined) {
+            //             return xhrGetResponseText.call(this);
+            //         } else {
+            //             return value;
+            //         }
+            //     },
+
+            //     // newValue => newValue,
+            //     function(newValue) { return newValue; }
+            // );
+
+            Object.defineProperty(xhr, "responseText", {
+                enumerable: true,
+                configurable: true,
+
+                get() {
+                    if (typeof this.__contentScript__responseText != "undefined") {
+                        return this.__contentScript__responseText;
+                    }
+
+                    return xhrGetResponseText.call(this);
+                },
+
+                set(newResponseText) {
+                    this.__contentScript__responseText = newResponseText;
+                }
+            });
+
+            let xhrSetOnreadystatechange = xhr.__lookupSetter__("onreadystatechange");
+
+            hookProperty(
+                xhr, "onreadystatechange",
+
+                function(callback) { return callback; },
+
+                function(callback) {
+                    let newCallback = function() {
+                        if (
+                            (this.responseType == "" || this.responseType == "text") &&
+                            this.__detoxify__url.startsWith(TWITTER_HOOK_PREFIX)
+                        ) {
+                            let response;
+
+                            try {
+                                response = JSON.parse(xhrGetResponseText.call(this));
+                            } catch (exc) {
+                                console.log("detoxify: JSON syntax error");
+                            }
+
+                            if (response != undefined) {
+                                walkTweets(response);
+                                this.responseText = JSON.stringify(response);
+                            }
+                        }
+
+                        return callback.apply(this, arguments);
+                    };
+
+                    xhrSetOnreadystatechange.call(this, newCallback);
+
+                    return newCallback;
+                }
+            );
+        })();
+    `;
+
+    document.head.prepend(shellcode);
 }
 
-const DETOXIFIED_ATTRIBUTE_NAME = "is-detoxified";
-
-document.addEventListener("onResponseInterception", () => {
-    scrapeData();
-});
-document.addEventListener("DOMContentLoaded", checkForDOM);
+requestIdleCallback(init);

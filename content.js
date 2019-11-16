fdc33f301bbadd521eacf1b07d716a3b860bfaf4
Roman Zhikharevich <rzhikharevich@gmail.com>
2019-11-16 23:14:28 +0200

    woof

--- content.js
+++ content.js
@@ -1,9 +1,41 @@
 function init() {
     if (document.body && document.head) {
+        installBackendApi();
         patchXhr();
     } else {
         requestIdleCallback(init);
     }
+}
+
+function installBackendApi() {
+    document.addEventListener("detoxifyTweetRequest", event => {
+        let request = event.detail;
+
+        detoxifyText(request.tweetText, backendResponse => {
+            let response = {tweetId: request.tweetId};
+
+            if ("text" in backendResponse) {
+                response.tweetText = backendResponse.text;
+            }
+
+            document.dispatchEvent(new CustomEvent("detoxifyTweetResponse", {detail: response}));
+        });
+    });
+}
+
+function detoxifyText(text, callback) {
+    let xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
+            let response = JSON.parse(xhr.responseText);
+            callback(response);
+        }
+    };
+
+    xhr.open("POST", "http://127.0.0.1:8080/detoxify", true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(JSON.stringify({"text": text}));
 }
 
 function patchXhr() {
@@ -11,11 +43,13 @@ function patchXhr() {
     shellcode.type = "text/javascript";
 
     shellcode.innerHTML = `
-        const TWITTER_HOOK_PREFIX = "https://api.twitter.com/2/timeline/profile/";
+        const TWITTER_HOOK_PREFIX = "https://api.twitter.com/2/timeline/";
 
         let nextTweetId = 0;
-        let idToText = {};
-        let idToElement = {};
+        let tweetIdToText = {};
+        let tweetIdToElement = {};
+
+        let observer;
 
         function hookProperty(obj, key, get_fun, set_fun) {
             let value;
@@ -37,31 +71,67 @@ function patchXhr() {
             );
         }
 
-        function detoxifyText(text, ) {
-            let xhr = new XMLHttpRequest();
-
-            xhr.onreadystatechange = () => {
-                if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
-                    let response = JSON.parse(xhr.responseText);
-
-                }
-            };
+        function makePlaceholder(seed) {
+            return "\u2800" + seed.toString();
         }
 
-        function walkTweets(tree, ids) {
+        function walkTweets(tree) {
             if (typeof tree != "object") {
                 return;
             }
 
+            if ("id_str" in tree) {
+                console.log(tree["id_str"]);
+            }
+
             for (let key in tree) {
                 if (key == "full_text") {
-                    ids.push(nextTweetId);
-                    idToText[nextTweetId] = tree[key];
-                    tree[key] = "__detoxify__" + nextTweetId.toString();
+                    try {
+                        decodeURIComponent(encodeURIComponent(tree[key]));
+                    } catch (exc) {
+                        console.log(tree[key]);
+                    }
+
+                    tweetIdToText[nextTweetId] = tree[key];
+
+                    let isStatus = location.href.includes("/status/");
+
+                    let username = tree[key].trim().match(/@[a-zA-Z0-9_]+/);
+
+                    if (username == null || !isStatus) {
+                        tree[key] = makePlaceholder(nextTweetId);
+                    } else {
+                        tree[key] = username + " " + makePlaceholder(nextTweetId);
+                    }
+
                     nextTweetId++;
                 } else {
                     walkTweets(tree[key]);
                 }
+            }
+        }
+
+        function loadTweetElements() {
+            let spans = document.getElementsByTagName("span");
+
+            for (let span of spans) {
+                let idStr = span.innerText.trim();
+                let fhtagnIndex = idStr.indexOf("\u2800");
+
+                if (fhtagnIndex < 0) {
+                    continue;
+                }
+
+                let id = parseInt(idStr.substr(fhtagnIndex + 1));
+
+                span.innerText = "⌛️";
+
+                tweetIdToElement[id] = span;
+
+                document.dispatchEvent(new CustomEvent(
+                    "detoxifyTweetRequest",
+                    {detail: {tweetId: id, tweetText: tweetIdToText[id]}}
+                ));
             }
         }
 
@@ -72,6 +142,7 @@ function patchXhr() {
 
             xhr.open = function(method, url) {
                 this.__detoxify__url = url;
+                console.log(url);
                 return xhrOpen.apply(this, arguments);
             };
 
@@ -88,7 +159,6 @@ function patchXhr() {
             //         }
             //     },
 
-            //     // newValue => newValue,
             //     function(newValue) { return newValue; }
             // );
 
@@ -136,7 +206,9 @@ function patchXhr() {
                             }
                         }
 
-                        return callback.apply(this, arguments);
+                        let ret = callback.apply(this, arguments);
+
+                        return ret;
                     };
 
                     xhrSetOnreadystatechange.call(this, newCallback);
@@ -144,6 +216,28 @@ function patchXhr() {
                     return newCallback;
                 }
             );
+
+            document.addEventListener("detoxifyTweetResponse", event => {
+                let response = event.detail;
+                let id = response.tweetId;
+
+                if ("tweetText" in response) {
+                    tweetIdToElement[id].innerText = response.tweetText;
+                } else {
+                    tweetIdToElement[id].innerText = tweetIdToText[id];
+                }
+            });
+
+            observer = new MutationObserver((mutations, observer) => {
+                loadTweetElements();
+            });
+
+            observer.observe(document.body, {
+                attributes: true,
+                childList: true,
+                subtree: true,
+                characterData: true
+            });
         })();
     `;
 
